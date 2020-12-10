686affc160b482c1e84a6e070282ca93aec8d736
jordiugarte <jordi933@gmail.com>
2020-12-10 09:11:15 -0400

    Update pull.php

--- pull.php
+++ pull.php
@@ -1,10 +1,8 @@
 <?php
-
-// Use in the “Post-Receive URLs” section of your GitHub repo.
-
 if ( $_POST['payload'] ) {
 shell_exec( 'git pull' );
 echo 'pull performed';
 }
-
+shell_exec( 'git pull' );
+echo 'pull performed';
 ?>
