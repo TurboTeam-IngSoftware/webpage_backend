8c3a2c1c413e563353ab2a08f41ed6e7a5aceaee
jordiugarte <jordi933@gmail.com>
2020-12-10 09:03:05 -0400

    Update pull.php

--- pull.php
+++ pull.php
@@ -2,8 +2,11 @@
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
-if ( $_POST['payload'] ) {
-shell_exec( ‘git reset –hard HEAD && git pull’ );
+if ( $_POST['PAYLOAD'] ) {
+shell_exec( 'git reset –hard HEAD && git pull' );
+echo 'pull performed';
 }
+shell_exec( 'git reset –hard HEAD && git pull' );
+echo 'pull performed';
 
 ?>hi
